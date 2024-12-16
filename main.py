6aa9abfa52b4ec0ced4d3c04ee297556ba8506d2
SergeyTsVL <tsarskiytsarskiy@mail.ru>
2024-12-16 11:44:15 +0700

    1

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ templates = Jinja2Templates(directory="templates")
 
 @app.get('/', response_class=HTMLResponse)
 async def home(request: Request):
+    return templates.TemplateResponse('base.html', {'request': request})
+
+@app.get('/home', response_class=HTMLResponse)
+async def home1(request: Request):
     return templates.TemplateResponse('home.html', {'request': request})
 # uvicorn main:app --reload
 
