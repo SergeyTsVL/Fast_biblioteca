a4ded541f301528101d8fb3b13040ff4228af8bf
SergeyTsVL <tsarskiytsarskiy@mail.ru>
2024-12-16 13:08:06 +0700

    1

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ from starlette.responses import HTMLResponse
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
-@app.get('/', response_class=HTMLResponse)
+@app.get('/')
 async def home(request: Request):
-    return templates.TemplateResponse('base.html', {'request': request})
-
-@app.get('/home', response_class=HTMLResponse)
-async def home1(request: Request):
     return templates.TemplateResponse('home.html', {'request': request})
+
+# @app.get('/home')
+# async def home1(request: Request):
+#     return templates.TemplateResponse('home.html', {'request': request})
 # uvicorn main:app --reload
 
